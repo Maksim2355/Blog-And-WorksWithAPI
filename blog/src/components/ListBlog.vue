2b91d99b714e005fe508a3f8bad9f3c9291cad40
Maksim provotorov <provotorov_maksim@mail.ru>
2019-12-11 22:49:34 +0300

    Web 0.1

---
+++ blog/src/components/ListBlog.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul>
+        <Post> </Post>
+        <Post> </Post>
+    </ul>
+</template>
+
+<script>
+    import Post from "@/components/Post";
+    export default {
+        name: "ListBlog",
+        components: {
+            Post
+        }
+    }
+</script>
+
+<style scoped>
+    ul {
+        list-style-type: none;
+        margin-top: 20px;
+        padding: 0;
+    }
+</style>
